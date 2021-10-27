a76658bb0ad3b67a6b90796ec2e5fc60eaecf973
Ferdy199 <fdumbadumba@gmail.com>
2021-10-27 23:18:34 +0700

    menambahkan enkripsi pada database

--- core/src/main/java/com/dicoding/movieapp/core/core_di/DatabaseModule.kt
+++ core/src/main/java/com/dicoding/movieapp/core/core_di/DatabaseModule.kt
@@ -6,15 +6,19 @@ import com.dicoding.movieapp.core.local.room.MovieDao
 import com.dicoding.movieapp.core.local.room.MovieDatabase
 import dagger.Module
 import dagger.Provides
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
     @Singleton
     @Provides
-    fun providesDatabase(context: Context): MovieDatabase = Room.databaseBuilder(
-        context, MovieDatabase::class.java, "Movies.db"
-    ).fallbackToDestructiveMigration().build()
+    fun providesDatabase(context: Context): MovieDatabase{
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        return Room.databaseBuilder(context, MovieDatabase::class.java, "Movies.db").fallbackToDestructiveMigration().openHelperFactory(factory).build()
+    }
 
     @Provides
     fun provideMovieDao(database: MovieDatabase): MovieDao = database.movieDao()
